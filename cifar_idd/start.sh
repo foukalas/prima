91ae1056944b4198246e08769b500ea3a7a59048
foukalas <57145302+foukalas@users.noreply.github.com>
2022-03-30 11:14:24 +0300

    Add files via upload

---
+++ cifar_idd/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+#python3 server.py;python3 ue_1/ue_1.py;python3 ue_2/ue_2.py;python3 ue_3/ue_3.py;python3 ue_4/ue_4.py
+
+python3 ue_1/ue_1.py;python3 ue_2/ue_2.py;python3 ue_3/ue_3.py;python3 ue_4/ue_4.py
